ab7578bbd65c866d538d32b9efedae2c6c87d53a
Ícaro Cainaã G. dos Santos <consultor.2556.204@7comm.com.br>
2023-03-06 11:46:10 -0300

    add button projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { AvatarComponent } from './components/avatar/avatar.component';
 import { UserInfoComponent } from './pages/user-info/user-info.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ProjectsComponent } from './pages/Projects/Projects.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, children: [
 
     {path: 'curriculo', component: UserInfoComponent},
     {path: 'contato', component: ContactComponent},
+    {path: 'projetos', component: ProjectsComponent},
 
   ]}
 ];
